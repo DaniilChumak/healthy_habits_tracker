c5f40ff80bb2bbd9e2fc485a54aeef633a613568
user <danilchumak80@gmail.com>
2024-10-18 16:38:51 +0300

    Написали контреллеры

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from habits.models import Habit
+from habits.paginators import MyPagination
+from habits.serializers import HabitSerializer
+from users.permissions import IsOwner
+
 
 # Create your views here.
+class HabitListAPIView(generics.ListAPIView):
+    """Habit list endpoint"""
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = MyPagination
+    #
+    # def get(self, request):
+    #     queryset = Habit.objects.all()
+    #     paginated_queryset = self.paginate_queryset(queryset)
+    #     serializer = HabitSerializer(paginated_queryset, many=True)
+    #     return self.get_paginated_response(serializer.data)
+
+
+class MyHabitListAPIView(HabitListAPIView):
+    """User's habit list endpoint"""
+
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        user = self.request.user
+        if not user.is_superuser:
+            queryset = Habit.objects.filter(owner=user)
+        else:
+            queryset = Habit.objects.all()
+        return queryset
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    """Habit create endpoint"""
+
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        habit = serializer.save()
+        habit.owner = self.request.user
+        habit.users.add(self.request.user)
+        habit.save()
+
+
+class HabitRetrieveAPIView(generics.RetrieveAPIView):
+    """Habit retrieve endpoint"""
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    """Habit update endpoint"""
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    """Habit delete endpoint"""
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+import secrets
+import string
+from datetime import datetime
+
+import pytz
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.urls import reverse
+from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+                                     ListAPIView, UpdateAPIView)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from config import settings
+from users.models import User
+from users.serializers import MyTokenObtainPairSerializer, UserSerializer
+
 
 # Create your views here.
+class UserCreateAPIView(CreateAPIView):
+    """Контроллер создания пользователя"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    """Контроллер авторизации пользователя"""
+    serializer_class = MyTokenObtainPairSerializer
+
+    def perform_authentication(self, request):
+        auth_header = request.headers.get("Authorization")
+        if auth_header:
+            try:
+                token = auth_header.split()[1]
+                user = User.objects.filter(verification_code=token).first()
+                if user:
+                    zone = pytz.timezone(settings.TIME_ZONE)
+                    user.last_login = datetime.now(zone)
+                    user.save()
+            except AttributeError as e:
+                print(e)
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    """Контроллер редактирования профиля пользователя"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserListAPIView(ListAPIView):
+    """Контроллер вывода списка пользователей"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    """Контроллер удаления пользователя"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+
+def verification_view(request, token):
+    """Функция верификации пользователя"""
+    user = User.objects.filter(verification_code=token).first()
+    if user:
+        user.is_active = True
+        user.save()
+    return redirect("users:login")
+
+
+def recover_password(request):
+    """Функция восстановления пароля"""
+    alphabet = string.ascii_letters + string.digits
+    password = "".join(secrets.choice(alphabet) for i in range(10))
+    request.user.set_password(password)
+    request.user.save()
+    message = f"Ваш новый пароль:\n{password}"
+    send_mail(
+        "Смена пароля",
+        message=message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email],
+        fail_silently=False,
+    )
+    return redirect(reverse("catalog:product_list"))
